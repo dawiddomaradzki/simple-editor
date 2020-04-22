09853f1f797b4b7e8331052166cea5a9c3c16685
Dawid Domaradzki <dawid-kun@wp.pl>
2020-04-22 17:55:50 +0200

    scheme and functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,25 @@
-"use strict";
-
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
-
 console.log(`Hello world!`);
 
 
+const inputText = document.querySelector('.editor__enter--js');
+const btnLoad = document.querySelector('.editor__button--load--js');
+const btnSave = document.querySelector('.editor__button--save--js');
+
+// let inputArray = localStorage.setItem('items', JSON.stringify(inputArray));
+
+
+
+btnSave.addEventListener('click', (e) => {
+  e.preventDefault();
+  const input = inputText.value;
+
+  console.log(input);
+  localStorage.setItem ('input', input);
+});
+
+
+btnLoad.addEventListener('click', (e) => {
+  e.preventDefault();
+  inputText.value = localStorage.getItem('input');
+
+});
